a6e169f4e67756ca91ade1d74552471cb90cef11
Eimar Ramos <eimar.ramos@lodisna.com>
2024-04-27 20:27:26 +0200

    feat: hero bubbels

--- src/app/pages/home/components/hero/hero.component.ts
+++ src/app/pages/home/components/hero/hero.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { LinkComponent } from '../../../../components';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [LinkComponent],
+  imports: [LinkComponent, NgFor],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  getNumberArray(n: number): Array<number> {
+    return Array(n);
+  }
+}

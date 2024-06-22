9d986603b4113f029f24e37ff087239dfe152585
Eimar Ramos <eimarramos.sacari@gmail.com>
2024-06-22 22:08:33 +0200

    refactor: Knowledge

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Section } from '../../shared';
 
 @Component({
   selector: 'app-section',
   standalone: true,
   imports: [],
   templateUrl: './section.component.html',
-  styleUrl: './section.component.css'
+  styleUrl: './section.component.css',
 })
 export class SectionComponent {
-
+  @Input() section?: Section;
 }
--- src/app/pages/home/components/knowledge/knowledge.component.ts
+++ src/app/pages/home/components/knowledge/knowledge.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../../../components';
+import { SectionComponent } from '../../../../components/section/section.component';
+import { Section } from '../../../../shared';
 
 @Component({
   selector: 'app-knowledge',
   standalone: true,
-  imports: [CardComponent],
   templateUrl: './knowledge.component.html',
   styleUrl: './knowledge.component.css',
+  imports: [CardComponent, SectionComponent],
 })
-export class KnowledgeComponent {}
+export class KnowledgeComponent {
+  knowledge: Section = {
+    title: 'ÁREAS DONDE TENGO EXPERIENCIA',
+    subtitle: 'Conocimientos',
+  };
+}

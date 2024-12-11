e24ac2527dbf55946d22fa77c922f111057847e5
Eimar <eimar.ramossacari@gmail.com>
2024-12-11 02:00:06 +0100

    fix: El proyecto en nulo al crear una Card

--- src/app/pages/home/components/projects/components/project-card/project-card.component.ts
+++ src/app/pages/home/components/projects/components/project-card/project-card.component.ts
@@ -1,22 +1,25 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
-import { AppComponent } from '../../../../../../app.component';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Project } from '../../types';
 import { Card, CardComponent } from '../../../../../../components';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [AppComponent, CardComponent],
+  imports: [CardComponent],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.css',
 })
-export class ProjectCardComponent {
-  @Input() project?: Project;
+export class ProjectCardComponent implements OnInit {
+  @Input() project!: Project;
 
-  project1: Card = {
-    title: 'Peritajes Web / App',
-    subtitle: 'Desarrollo de una aplicación web',
-  };
+  project1?: Card;
+
+  ngOnInit(): void {
+    this.project1 = {
+      title: this.project.title,
+      subtitle: this.project.summary,
+    };
+  }
 
   openModal(event: MouseEvent) {
     const buttonClicked = event.target as HTMLButtonElement;
